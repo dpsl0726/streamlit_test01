072f0c60c2aff88109d5212e959e434f14406cae
dpsl0726 <133926006+dpsl0726@users.noreply.github.com>
2023-05-18 12:05:09 +0900

    Update Multipage_Home.py

--- Multipage_Home.py
+++ Multipage_Home.py
@@ -16,7 +16,7 @@ from PIL import Image
 st.set_page_config(layout="wide")
 
 
-st.title("-------------------🐶서울특별시 반려동물 종합 플랫폼🐶---------------------")
+st.title("🐶서울특별시 반려동물 종합 플랫폼🐶")
 
 image_local = Image.open('./a.png')
 st.image(image_local, width=1200)
